5b54f4ad9f396ad498f7dba1e19257c82823ddd2
Yash <yk6819@srmist.edu.in>
2026-02-16 09:32:50 +0530

    Initial commit

---
+++ java/OOPS_banner.java
@@ -0,0 +1,11 @@
+gpublic class OOPS_banner {
+    public static void main(String[] args) {
+        System.out.println("   ***   " + "  " + "   ***    " + "  " + "******  " + "  " + "  *****    " );
+        System.out.println(" **   ** " + "  " + " **   **  " + "  " + "**    **" + "  " + " **    **  " );
+        System.out.println("**     **" + "  " + "**     ** " + "  " + "**    **" + "  " + " **        " );
+        System.out.println("**     **" + "  " + "**     ** " + "  " + "******  " + "  " + "  *****    " );
+        System.out.println("**     **" + "  " + "**     ** " + "  " + "**      " + "  " + "       **  " );
+        System.out.println(" **  **  " + "  " + " **   **  " + "  " + "**      " + "  " + " **    **  " );
+        System.out.println("   ***   " + "  " + "   ***    " + "  " + "**      " + "  " + "  *****    " );
+    }
+}
